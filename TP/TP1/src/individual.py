bcf3e448305c725ff758b23cf6efca5a00e420ca
ignitz <ignitzhjfk@gmail.com>
2018-09-12 05:05:43 -0300

    step

--- TP/TP1/src/individual.py
+++ TP/TP1/src/individual.py
@@ -1,35 +1,74 @@
 import numpy as np
+import hashlib as hl
+from utils import *
 
 class Node:
-    def __init__(self):
-        raise NotImplementedError
+    """
+    doc
+    """
+    def __init__(self, node):
+        self.children = node
     
-    def eval(self):
-        raise NotImplementedError
+    def eval(self, values):
+        return self.children.eval(values)
+    
+    def walk(self):
+        response = self.children.walk(self, 0)
+        if type(response) is not list:
+            response = [response]
+        return response
+    
+    def __str__(self):
+        return self.children.__str__()
+
+    def get_unique_id(self):
+        """
+        Return shasum to detect identical individual
+        """
+        string = self.__str__()
+        return hl.sha256(string.encode('utf-8')).hexdigest()
+
 
 class Constant(Node):
+    """
+    doc
+    """
     value = 0.0
 
     def __init__(self, value):
-        assert(type(value) is float or np.array)
-        self.value = value
+        assert(type(value) is int or type(value) is float or type(value) is np.array or type(value) is np.matrix)
+        self.value = float(value)
 
-    def eval(self, values):
+    def eval(self, values=None):
         return self.value
+    
+    def walk(self, parent, depth):
+        return (parent, self, self.get_unique_id(), depth + 1)
+    
+    def __str__(self):
+        return str(np.around(self.value, decimals=2))
+
 
 class Operator(Node):
-    operator = None
-    node_left = None
-    node_right = None
+    """
+    doc
+    """
 
     def __init__(self, operator, node_left, node_right):
         assert(type(operator) is str)
         assert(isinstance(node_left, Node))
         assert(isinstance(node_right, Node))
+        if operator not in ['add', 'sub', 'mul', 'div']:
+            operator = {
+                '+': 'add',
+                '-': 'sub',
+                '*': 'mul',
+                'x': 'mul',
+                '/': 'div',
+            }[operator]
         self.operator = operator
         self.node_left = node_left
         self.node_right = node_right
-        print(self.node_left.eval(np.array([1.0,2.0])), self.node_right.eval(np.array([1.0,2.0])))
     
     def handle_div(self, values):
         """
@@ -37,24 +76,61 @@ class Operator(Node):
 
         """
         if self.node_right.eval(values) == 0.0:
-            return 100000000000000.0
+            return float('inf')
         else:
             return self.node_left.eval(values) / self.node_right.eval(values)
     
     def eval(self, values):
+        a = self.node_left.eval(values)
+        b = self.node_right.eval(values)
+        if np.isnan(a) or np.isnan(b) or a == np.inf or b == np.inf:
+            return float('inf')
+        if self.operator == 'add':
+            return a + b
+        elif self.operator == 'sub':
+            return a - b
+        elif self.operator == 'mul':
+            return a * b
+        elif self.operator == 'div':
+            divisor = b
+            if divisor == 0.0:
+                return float('inf')
+            return a / divisor
+        else:
+            raise 'operator invalid'
+    
+    def walk(self, parent, depth):
+        a = self.node_left.walk(self, depth + 1)
+        if type(a) is not list:
+            response = [a]
+        else:
+            response = a
+        b = self.node_right.walk(self, depth + 1)
+        if type(b) is not list:
+            response += [b]
+        else:
+            response += b
+        response += [(parent, self, self.get_unique_id(), depth + 1)]
+        return response
+    
+    def get_operator_char(self):
         return {
-            'add': self.node_left.eval(values) + self.node_right.eval(values),
-            'sub': self.node_left.eval(values) - self.node_right.eval(values),
-            'mul': self.node_left.eval(values) * self.node_right.eval(values),
-            'div': self.handle_div(values)
+            'add': '+',
+            'sub': '-',
+            'mul': 'x',
+            'div': '/'
         }[self.operator]
 
-"""
-Index?
-multi variable?
-"""
+    def __str__(self):
+        return '(' + self.node_left.__str__() + \
+            ' ' + self.get_operator_char() + ' ' + \
+            self.node_right.__str__() + ')'
+
 
 class Variable(Node):
+    """
+    doc
+    """
     def __init__(self, dim):
         self.dim = dim
     
@@ -62,21 +138,200 @@ class Variable(Node):
         return self.dim
 
     def eval(self, values):
-        return values[self.dim]
+        return values.item(self.dim)
+    
+    def walk(self, parent, depth):
+        return (parent, self, self.get_unique_id(), depth + 1)
+
+    def __str__(self):
+        return 'X' + str(self.dim)
 
 
 class Function(Node):
-    def __init__(self):
-        raise NotImplementedError
+    """
+    Abstract class
+    f(x)
+    """
+    def __init__(self, node):
+        self.node = node
+
+    def walk(self, parent, depth):
+        a = self.node.walk(self, depth + 1)
+        if type(a) is not list:
+            response = [a]
+        else:
+            response = a
+        return response + [(parent, self, self.get_unique_id(), depth + 1)]
     
+
 class Sin(Function):
+    """
+    sin(x_i)
+    """
+    def eval(self, values):
+        return np.sin(self.node.eval(values))
+
+    def __str__(self):
+        if type(self.node) is Operator:
+            return 'sin' + str(self.node)
+        else:
+            return 'sin(' + str(self.node) + ')'
+        
+
+class Cos(Function):
+    """
+    cos(x_i)
+    """
+    def eval(self, values):
+        return np.cos(self.node.eval(values))
+    
+    def __str__(self):
+        if type(self.node) is Operator:
+            return 'cos' + str(self.node)
+        else:
+            return 'cos(' + str(self.node) + ')'
 
 
+class Exp(Function):
+    """
+    e^(x_i)
+    """
+    def eval(self, values):
+        return np.exp(self.node.eval(values))
+    
+    def __str__(self):
+        if type(self.node) is Operator:
+            return 'exp' + str(self.node)
+        else:
+            return 'exp(' + str(self.node) + ')'
 
-def main():
+
+class Ln(Function):
+    """
+    ln(x_i)
+    if x <= 0 then return inf
+    """
+    def eval(self, values):
+        eval_value = self.node.eval(values)
+        if eval_value <= 0.0:
+            return float('inf')
+        else:
+            return np.log(eval_value)
+    
+    def __str__(self):
+        if type(self.node) is Operator:
+            return 'ln' + str(self.node)
+        else:
+            return 'ln(' + str(self.node) + ')'
+
+
+def generate_operator(n_dim, depth):
+    poss_operators = [0, 0, 0, 1, 1, 2, 2, 3]
+    index = poss_operators[np.random.random_integers(0, len(poss_operators)-1)]
+    operator = ['add','sub','mul','div'][index]
+    node_left = generate_subtree(n_dim, depth-1)
+    node_right = generate_subtree(n_dim, depth-1)
+    # node_left = Sin(0)
+    # node_right = Sin(0)
+    # adjust to precedence to remove ambig...
+    typeA = type(node_left)
+    typeB = type(node_right)
+    if (typeA is Constant) and (typeB is Constant):
+        return Constant(Operator(operator, node_left, node_right).eval(0.0))
+
+    if operator in ['add', 'sub', 'mul']:
+        if typeB is Constant:
+            if (typeA is not Constant):
+                node_left, node_right = node_right, node_left
+        elif typeB is Variable:
+            if (isinstance(node_left, Function)):
+                node_left, node_right = node_right, node_left
+        else: # is instance of Function
+            if typeB is Sin:
+                if (typeA is Cos) or (typeA is Exp) or (typeA is Ln):
+                    node_left, node_right = node_right, node_left
+            if typeB is Cos:
+                if (typeA is Exp) or (typeA is Ln):
+                    node_left, node_right = node_right, node_left
+            if typeB is Exp:
+                if typeA is Ln:
+                    node_left, node_right = node_right, node_left
+    
+    if operator in ['add', 'sub']:
+        # try to force precedence 'parenteses' to the left
+        if type(node_right) is Operator:
+            if node_right.operator in ['add', 'sub']:
+                operator, node_right.operator = node_right.operator, operator
+                
+                auxA, auxB, auxC = node_left, node_right.node_left, node_right.node_right
+
+                node_left = node_right
+                node_right = auxC
+                node_left.node_left = auxA
+                node_left.node_right = auxB
+
+        # factor to 2 times a node
+        if type(node_left) is type(node_right):
+            if node_left.get_unique_id() == node_right.get_unique_id():
+                if operator == 'add':
+                    node_left = Constant(2)
+                else:
+                    node_left = Constant(-2)
+                operator = 'mul'
+    
+    if operator == 'div':
+        if node_left.get_unique_id() == node_right.get_unique_id():
+            return Constant(1)
+        
+        if type(node_right) is Constant:
+            if node_right.eval() == 1.0:
+                return node_left
+
+    return Operator(operator, node_left, node_right)
+
+def generate_terminal(n_dim):
+    choose = np.random.random_integers(0, 5)
+    if choose == 1:
+        return Variable(np.random.random_integers(0, n_dim - 1))
+    else:
+        a = np.random.normal(scale=10)
+        if a > 0:
+            return Operator('sub', Variable(np.random.random_integers(0, n_dim - 1)), Constant(a))
+        else:
+            return Operator('add', Variable(np.random.random_integers(0, n_dim - 1)), Constant(-a))
+
+def generate_subtree(n_dim, depth):
+    """
+    Generate a random math expression
+    """
+
+    if depth <= 0:
+        return generate_terminal(n_dim)
+
+    choose = np.random.random_integers(0, depth + 8)
+
+    if 0 <= choose <= depth:
+        return generate_operator(n_dim, depth - 1)
+    elif depth < choose <= depth + 3:
+        return Variable(np.random.random_integers(0,n_dim - 1))
+    elif choose == depth + 4:
+        return Constant(np.random.normal(scale=10))
+    elif choose == depth + 5:
+        return Sin(generate_terminal(n_dim))
+    elif choose == depth + 6:
+        return Cos(generate_terminal(n_dim))
+    elif choose == depth + 7:
+        return Exp(generate_terminal(n_dim))
+    elif choose == depth + 8:
+        return Ln(generate_terminal(n_dim))
+
+def generate_individual(n_dim, depth=7):
+    return Node(generate_subtree(n_dim, depth))
+    
+def test_basic_operations():
     x1 = Variable(0)
     x2 = Variable(1)
-    values = np.array([1.0, 2.0])
+    values = np.array([1.0, 0.0])
     print(x1.eval(values), x2.eval(values))
     node = Operator('add', x1, x2)
     print(node.eval(values))
@@ -87,5 +342,37 @@ def main():
     node = Operator('div', x1, x2)
     print(node.eval(values))
 
-if __name__ == '__main__':
-    main()
+def test_functions():
+    sin = Sin(0)
+    cos = Cos(0)
+    exp = Exp(0)
+    ln = Ln(0)
+    for x in range(-10, 105, 5):
+        values = [x/10]
+        print_blue("Sin(" + str(values[0]) + ") = " + str(sin.eval(values)))
+        print_blue("Cos(" + str(values[0]) + ") = " + str(cos.eval(values)))
+        print_blue("E(" + str(values[0]) + ") = " + str(exp.eval(values)))
+        print_blue("LN(" + str(values[0]) + ") = " + str(ln.eval(values)))
+
+def test_print_and_operation():
+    a = Constant(1)
+    b = Constant(2)
+    foo = Operator('add', a, b)
+    print(foo)
+    a = Ln(0)
+    b = Cos(1)
+    bar = Operator('+', a, b)
+    print(bar, ' = ', np.around(bar.eval([np.pi/2, 0]), decimals=2))
+
+def test():
+
+    for i in range(5):
+        individual = generate_individual(2)
+        print(individual.get_unique_id(), individual)
+    # test_print_and_operation()
+
+# def main():
+#     test()
+
+# if __name__ == '__main__':
+#     main()
--- TP/TP1/src/main.py
+++ TP/TP1/src/main.py
@@ -3,64 +3,202 @@ import argparse
 import os
 import sys
 
+from utils import *
+
+from individual import *
+
 DATASET_FOLDER = 'datasets'
 DATASET_NAMES = list()
 
-"""
-
-initial population
-
-
-while true
-    fitness
-    selection
-        crossover
-        reprodution
-        mutation
-    new population
-    stop criteria
-
-best solution
-"""
-
-"""
-Estatsticas importantes
-Estas estatsticas devem ser coletadas para todas as gerac~oes.
-1. Fitness do melhor e pior indivduos
-2. Fitness media da populac~ao
-3. Numero de indivduos repetidos na populac~ao
-4. Numero de indivduos gerados por cruzamento melhores e piores que a tness media
-dos pais
-O que deve ser entregue...
- Codigo fonte do programa
- Documentac~ao do trabalho:
-{ Introduc~ao
-{ Implementac~ao: descric~ao sobre a implementac~ao do programa, incluindo detalhes
-da representac~ao, tness e operadores utilizados
-{ Experimentos: Analise do impacto dos par^ametros no resultado obtido pelo AE.
-{ Conclus~oes
-{ Bibliograa
-"""
+# """
+# Estatísticas importantes
+# Estas statísticas devem ser coletadas para todas as gerações.
+# 1. Fitness d melhor e pior indivíduos
+# 2. Fitness média da população
+# 3. Número de indivíduos repetidos na população
+# 4. Número de indivíduos gerados por cruzamento melhores e piores que a tness média
+# dos pais
+# O que deve ser entregue...
+#  Código fonte do programa
+#  Documentação do trabalho:
+# { Introdução
+# { Implementação: descrição sobre a implementação do programa, incluindo detalhes
+# da representação, tness e operadores utilizados
+# { Experimentos: Análise do impacto dos parâmetros no resultado obtido pelo AE.
+# { Conclusões
+# { Bibliograa
+# """
 
 class GeneticProgramming(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, population=50, n_dim=1, depth=7):
+        self.population = population
+        self.n_dim = n_dim
+        self.depth = depth
 
-    def fitness(self):
-        pass
-    
-    def crossover(self):
-        pass
-    
+        self.individuals = list()
+
+        for _ in range(population):
+            self.individuals.append(generate_individual(n_dim, depth))
+
+    def fitness(self, train_data):
+        """
+        NRMSE
+        """
+        Ymean = train_data[:, -1].mean()
+        normalize = train_data[:, -1] - Ymean
+        normalize = np.sum(normalize)
+
+        nrmse_ind = list()
+        
+        index = 0
+        for ind in self.individuals:
+            real_diff = list()
+            for data in train_data:
+                eval_value = ind.eval(data[:,:-1])
+                real_value = data[:,-1].item(0)
+                real_diff.append((eval_value - real_value)**2)
+            if float('inf') in real_diff:
+                nrmse_ind.append([float('inf'), index])
+            else:
+                nrmse_ind.append([np.sqrt(np.sum(real_diff)/normalize), index])
+            index += 1
+        nrmse_ind.sort(key=lambda x: x[0])
+        nrmse_ind = np.matrix(nrmse_ind)
+
+        indexes_ord = nrmse_ind[:,1].T.astype(int).tolist()[0]
+
+        # reorder individuals by fitness
+        self.individuals = [ self.individuals[i] for i in indexes_ord ]
+        
+        return nrmse_ind[:,0].T.tolist()
+
     def reproduction(self):
         pass
     
-    def mutation(self):
-        pass
+    def cross(self, choice1, choice2):
+        # print_warning('cross')
+        parent1, node1, id1, _ = choice1
+        parent2, node2, id2, _ = choice2
+
+        # print_warning(node1)
+        # print_warning(node2)
+
+        if type(parent1) is Node:
+            parent1.children = node2
+        elif type(parent1) is Operator:
+            idleft = parent1.node_left.get_unique_id()
+            idright = parent1.node_right.get_unique_id()
+            if idleft == id1:
+                parent1.node_left = node2
+            elif idright == id1:
+                parent1.node_right = node2
+            else:
+                raise 'Algo deu muito errado'
+        elif isinstance(parent1, Function):
+            parent1.node = node2
+        
+        if type(parent2) is Node:
+            parent2.children = node1
+        elif type(parent2) is Operator:
+            idleft = parent2.node_left.get_unique_id()
+            idright = parent2.node_right.get_unique_id()
+            if idleft == id2:
+                parent2.node_left = node1
+            elif idright == id2:
+                parent2.node_right = node1
+            else:
+                raise 'Algo deu muito errado'
+        elif isinstance(parent2, Function):
+            parent2.node = node1
+        
+
+
+    def crossover(self, prob_c=0.5, k = 2):
+        """
+        Troca sub-arvores
+        """
+        # print_warning('crossover')
+        indexes, inds = self.tournament(k)
+        ind1, ind2 = inds[:2]
+
+        if np.random.random() < prob_c:
+            ind1_uniques = ind1.walk()
+            ind2_uniques = ind2.walk()
+
+            x = np.random.choice(list(range(len(ind1_uniques))))
+            y = np.random.choice(list(range(len(ind2_uniques))))
+            choice1 = ind1_uniques[x]
+            choice2 = ind2_uniques[y]
+            self.cross(choice1, choice2)
+        
+        return ind1, ind2         
     
-    def new_population(self):
-        pass
+    def tournament(self, many=2):
+        """
+        Espera-se que a fitness já foi calculada a espera que os individuos
+        já estejam ordernados
+        retorna com individuos por ordem de fitness
+        """
+        # print_warning('tour')
+        possible_choices = list(range(0, self.population))
+        choices = list()
+        for _ in range(many):
+            choice = possible_choices.pop(np.random.random_integers(0, len(possible_choices)-1))
+            choices.append(choice)
+        choices.sort()
+        return choices, [self.individuals[i] for i in choices]
+
+    
+    def mutation(self, prob_mut=0.5, k = 2):
+        # print_warning('mutation')
+        indexes, inds = self.tournament(k)
+        ind = inds[0]
+
+        if np.random.random() < prob_mut:
+            # print(ind)
+
+            ind_uniques = ind.walk()
+            x = np.random.choice(list(range(len(ind_uniques))))
+            choice1 = ind_uniques[x]
+            
+            parent1, node1, id1, depth = choice1
+            # print_purple(node1)
+
+            if type(parent1) is Node:
+                parent1.children = generate_subtree(self.n_dim, self.depth)
+            elif type(parent1) is Operator:
+                idleft = parent1.node_left.get_unique_id()
+                idright = parent1.node_right.get_unique_id()
+                if idleft == id1:
+                    parent1.node_left = generate_subtree(self.n_dim, self.depth - depth)
+                elif idright == id1:
+                    parent1.node_right = generate_subtree(self.n_dim, self.depth - depth)
+                else:
+                    raise 'Algo deu muito errado'
+                # add optimizer
+                # (((X1 / 3.01) + (exp(X1 - 0.32) + (sin(X1 - 3.76) + ((X0 + 8.34) + X1)))) + 12.53)
+                # ((X1 / 3.01) + (exp(X1 - 0.32) + (sin(X1 - 3.76) + ((X0 + 8.34) + X1))))
+                # (4.99 + 12.53)
+            elif isinstance(parent1, Function):
+                parent1.node = generate_subtree(self.n_dim, self.depth - depth)
+            
+            # print_warning(ind)
+            # self.show_ind()
+        return ind
+
+    
+    def new_population(self, choosen_inds=[]):
+        # print_warning('newpop')
+        self.individuals = choosen_inds
+        for i in range(self.population - len(choosen_inds)):
+            # TODO: pais e filhos (nao do Renato Russo)
+            self.individuals.append(generate_individual(self.n_dim, self.depth))
+    
+    def show_ind(self):
+        print_green('Unique ID (SHA256)\tExpression')
+        for ind in self.individuals:
+            print_blue(str(ind.get_unique_id())[:13] + '...\t' + ind.__str__())
 
 
 def read_csv(filename):
@@ -88,18 +226,37 @@ def search_dataset_names():
     for name in names:
         DATASET_NAMES.append(name)
 
-def proto_debug(set_name):
-    train_data, test_data = read_csv(set_name)
-
-    print(train_data)
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("echo", help="echo the string you use here")
-    # args = parser.parse_args()
-    # print(args.echo)
     search_dataset_names()
-    proto_debug(DATASET_NAMES[1])
+    train_data, test_data = read_csv(DATASET_NAMES[2])
+
+    # description = "Genetic Programming using math expression trees"
+    # parser = argparse.ArgumentParser(description=description)
+    # parser.add_argument('population', type=int, help="number of individuals per epoch", default=50)
+    # args = parser.parse_args()
+    # GeneticProgramming(population=args.population, )
+    gp = GeneticProgramming(population=50, n_dim=train_data.shape[1]-1, depth=7)
+    gp.fitness(train_data)
+
+    count = 0
+    while True:
+        choosen_inds = list()
+        x,y = gp.crossover(prob_c=1.0, k=3)
+        choosen_inds.append(x)
+        choosen_inds.append(y)
+        x = gp.mutation(prob_mut=1.0, k=3)
+        choosen_inds.append(x)
+        gp.new_population(choosen_inds)
+        best = gp.fitness(train_data)[0]
+        print_blue(gp.individuals[0])
+        print(best[0])
+        count += 1
+        if count == 50:
+            break
+
+    gp.show_ind()
+
 
 if __name__ == '__main__':
     main()
