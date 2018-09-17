b2bdcd389eb8282437ee5e7e8e381dc4d5ba114c
Yuri Niitsuma <yuriniitsuma@MacBook-Pro-de-Yuri.local>
2018-09-17 19:24:56 -0300

    batch files

--- TP/TP1/src/run.sh
+++ TP/TP1/src/run.sh
@@ -1,1 +1,16 @@
-@python main.py synth1 -p 100 -k 2 -g 10 -c 0.45 -m 0.45
+# python main.py synth1 -p 100 -k 2 -g 10 -c 0.45 -m 0.45
+
+for pop in 50 100 500; do
+	for gen in 50 100 500; do
+		for k in 2 3 4 5 6 7; do
+			for c in 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+				for m in 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+					python main.py keijzer7 -p $pop -k $k -g $gen -c $c -m $m
+					# @python main.py synth1 -p $pop -k $k -g $gen -c $c -m $m
+					# @python main.py synth2 -p $pop -k $k -g $gen -c $c -m $m
+					# @python main.py concrete -p $pop -k $k -g $gen -c $c -m $m
+				done
+			done
+		done
+	done
+done
