8d98b4a7ed5cf9b0711d59b883b71f0b19608a93
ignitz <ignitzhjfk@gmail.com>
2018-09-21 00:56:57 -0300

    updated code

---
+++ TP/TP1/src/plotar.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+def read_file(pathname):
+    aux_data = list()
+    with open(pathname, 'r') as f:
+        while True:
+            line = f.readline()
+            if len(line) == 0:
+                break
+            aux_data.append(list(map(float, line.split(','))))
+    return np.matrix(aux_data)
+
+def load_data(dataname, p, g, k, c, m):
+    count = 1
+    filename = 'output/' + dataname
+    filename += '_p' + str(p) + '_g' + str(g)
+    filename += '_k' + str(k) + '_c' + str(c) + '_m' + str(m)
+    filename += '_' + str(count).zfill(3) + '.csv'
+    
+    count += 1
+    data = read_file(filename)
+    while count <= 5:
+        filename = 'output/' + dataname
+        filename += '_p' + str(p) + '_g' + str(g)
+        filename += '_k' + str(k) + '_c' + str(c) + '_m' + str(m)
+        filename += '_' + str(count).zfill(3) + '.csv'
+        data += read_file(filename)
+        count += 1
+    return (data/5)
+
+# data = load_data('synth1', 50, 50, 2, 0.3, 0.3)
+# primeiro tem que testar com 50,50
+# variando mutacao e crossover
+# depois varia o k
+
+def main():
+    EIXOS = [0, 55, 0, 1.5]
+
+    fig, ax = plt.subplots()
+
+    for x in [0.05, 0.1, 0.3, 0.5, 0.7, 0.9]:
+        generations = 50
+        data = load_data('synth1', 50, generations, 2, x, 0.05)
+        label = 'P(cross) = ' + str(x)
+        ax.plot(np.arange(0, generations + 1, 1), data[:,2][:], 'o', label=label)
+
+    title = 'Variação do Crossover'
+    ax.set(xlabel='Gerações', ylabel='Fitness médio',
+           title=title)
+    ax.axis(EIXOS)
+    ax.legend()
+    ax.grid()
+
+    fig.savefig("varcrossover.pdf")
+    # plt.show()
+
+    # AAAAAAAAAAAAAAAAA
+    fig, ax = plt.subplots()
+
+    for x in [0.05, 0.1, 0.3, 0.5, 0.7, 0.9]:
+        generations = 50
+        data = load_data('synth1', 50, generations, 2, 0.05, x)
+        label = 'P(mut) = ' + str(x)
+        ax.plot(np.arange(0, generations + 1, 1), data[:,2][:], 'o', label=label)
+
+    title = 'Variação da Mutação'
+    ax.set(xlabel='Gerações', ylabel='Fitness médio',
+           title=title)
+    ax.axis(EIXOS)
+    ax.legend()
+    ax.grid()
+
+    fig.savefig("varmutation.pdf")
+    # plt.show()
+
+    # AAAAAAAAAAAAAAAAA
+    fig, ax = plt.subplots()
+
+    for x in [2,3,4,5,6,7]:
+        generations = 50
+        data = load_data('synth1', 50, generations, x, 0.3, 0.6)
+        label = 'k = ' + str(x)
+        ax.plot(np.arange(0, generations + 1, 1), data[:,2][:], 'o', label=label)
+
+    title = 'Variação da K'
+    ax.set(xlabel='Gerações', ylabel='Fitness médio',
+           title=title)
+    ax.axis(EIXOS)
+    ax.legend()
+    ax.grid()
+
+    fig.savefig("varK.pdf")
+    # plt.show()
+
+
+    # merge data
+    # 
+
+if __name__ == '__main__':
+    main()
