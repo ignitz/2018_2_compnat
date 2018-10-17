85fc4d348e48a31a310d1faf7f6525dca6e33a37
Yuri Niitsuma <ignitzhjfk@gmail.com>
2018-10-17 03:15:56 -0300

    remove a lot of files

--- TP/TP2/src/main.cpp
+++ TP/TP2/src/main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int main(int argc, char *argv[])
+class Ant
+{
+public:
+    Ant ();
+    virtual ~Ant ();
+
+private:
+    int currV, nextV;
+    std::vector<int> tabu;
+    std::vector<int> path;
+
+    double tourLength;
+};
+
+class Graph
+{
+public:
+    Graph ();
+    virtual ~Graph ();
+
+private:
+    std::unordered_map<int, int> edge;
+};
+
+
+Graph::Graph () {
+    
+}
+Graph::~Graph () {
+}
+
+Ant::Ant() {
+    std::cout << "Ant" << std::endl;
+}
+Ant::~Ant() {
+    std::cout << "Desc Ant" << std::endl;
+}
+                          
+int main()
 {
     std::cout << "HelloWorld" << std::endl;
     return 0;
